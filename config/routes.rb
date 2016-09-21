79f10e6312354bf19bedce20e129a4508be15c00
howardcch <howard60915@gmai.com>
2016-09-21 15:38:27 +0800

    Update view & controller of Index

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
 	get "welcome" => "welcome#index"
 	root "welcome#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  	match ':controller(/:action(/:id(.:format)))', :via => :all
+  	match ':controller(/:action(/:id(.:format)))', :via => :all #外卡路由
 end
