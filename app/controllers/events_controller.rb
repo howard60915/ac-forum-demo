ed585ceb443b47cbe514e99cb813821621d2db0c
howardcch <howard60915@gmai.com>
2016-09-22 00:53:09 +0800

    Flash decoration

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,8 +1,10 @@
 class EventsController < ApplicationController
+	
+	before_action :set_event, :only => [:show, :edit, :update, :destroy]
 	#GET/events/index
 	#GET/events
 	def index
-		@events = Event.all
+		@events = Event.page(params[:page]).per(15)
 	end	
 	#GET/events/new，因為是新生成，所以name & description內都是空的
 	def new
@@ -10,24 +12,29 @@ class EventsController < ApplicationController
 	end	
 	#GET events/show/:id，因為是用find method，所以name & description會有原先event的資料
 	def show
-        @event = Event.find(params[:id])
+        
         @page_title = @event.name
 	end	
 	#GET events/edit/:id
 	def edit
-		@event = Event.find(params[:id])
 	end	
 
 	def update
-	  @event = Event.find(params[:id])
-	  @event.update(event_params)
+	  	if @event.update(event_params)
 
-	  redirect_to :action => :show, :id => @event
+	  		flash[:notice] = "編輯成功"
+
+	   		redirect_to :action => :show, :id => @event #到show頁面時，還是腰給一個id，show才能找到更新的資料
+		
+		else 
+		    render :action => :edit
+		end    	
 	end
 
 	def destroy
-	  @event = Event.find(params[:id])
-	  @event.destroy
+		@event.destroy
+
+		flash[:alert] = "刪除成功"
 
 	  redirect_to :action => :index
 	end
@@ -35,12 +42,21 @@ class EventsController < ApplicationController
 	#POST/events/create
 	def create
 		@event = Event.new( event_params )
-		@event.save
+		if @event.save
 
-		redirect_to :action => :index #告訴瀏覽器  HTTP code 303
+			flash[:notice] = "新增成功"
+			
+			redirect_to :action => :index #告訴瀏覽器  HTTP code 303
+		else
+			render :action => :new #new.html.erb
+		end	
 	end	
 
 	private
+
+	def set_event
+		@event = Event.find(params[:id])
+	end	
 	#params是屬於白名單（正面表列），只有params裡面認定的才會接起來
 	def event_params
 		params.require(:event).permit( :name, :description)
