c941e40472e9a83a7fadc3379f82b9c61159a1e3
陳璟皓 <howard60915@gmail.com>
2016-09-27 03:43:38 +0800

    CRUD Complete

--- app/controllers/event_attendees_controller.rb
+++ app/controllers/event_attendees_controller.rb
@@ -6,8 +6,31 @@ class EventAttendeesController < ApplicationController
 	def new
 		@attendee = @event.attendees.build		
 	end	
+	def create
+		@attendee = @event.attendees.build
+		@attendee.save
 
+		redirect_to event_attendees_path(@event,@attendee)
+	end
+	def show
+		@attendee = @event.attendees.find(params[:id]) 
+	end
+	def edit
+		@attendee = @event.attendees.find(params[:id])
+	end
+	def update
+		@attendee = @event.attendees.find(params[:id])
+		@attendee.update(attendees_params)
 
+		redirect_to event_attendee_path(@event)
+	end
+
+	def destroy
+		@attendee = @event.attendees.find(params[:id])
+		@attendee.destroy
+
+		redirect_to event_attendees_path(@event)
+	end
 
 
 
@@ -19,5 +42,8 @@ class EventAttendeesController < ApplicationController
 
 	def set_event
 		@event = Event.find(params[:event_id])
+	end
+	def attendees_params
+		params.require(:attendee).permit(:name, :event_id)
 	end
 end
