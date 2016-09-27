703bd9157ce11a204b256b5557267e2caedf68d0
陳璟皓 <howard60915@gmail.com>
2016-09-28 00:50:58 +0800

    many to many Practice Complete

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,6 +2,8 @@ class Event < ApplicationRecord
 	validates_presence_of :name
 	has_many :attendees
 	belongs_to :category
-
+	
 	delegate :name, :to => :category , :prefix => true, :allow_nil => true
+	has_many :event_groupships
+	has_many :groups, :through => :event_groupships
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160927094443) do
+ActiveRecord::Schema.define(version: 20160927134900) do
 
   create_table "attendees", force: :cascade do |t|
     t.string   "name"
@@ -25,6 +25,15 @@ ActiveRecord::Schema.define(version: 20160927094443) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "event_groupships", force: :cascade do |t|
+    t.integer  "event_id"
+    t.integer  "group_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["event_id"], name: "index_event_groupships_on_event_id"
+    t.index ["group_id"], name: "index_event_groupships_on_group_id"
+  end
+
   create_table "events", force: :cascade do |t|
     t.string   "name"
     t.text     "description"
@@ -35,6 +44,12 @@ ActiveRecord::Schema.define(version: 20160927094443) do
     t.string   "status"
     t.integer  "category_id"
     t.index ["category_id"], name: "index_events_on_category_id"
+  end
+
+  create_table "groups", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "people", force: :cascade do |t|
