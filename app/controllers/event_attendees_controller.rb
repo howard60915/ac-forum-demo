6a9ef5e1c52bc3fc8da9b92ea024a259e12596ca
陳璟皓 <howard60915@gmail.com>
2016-09-27 11:27:25 +0800

    Update Bootstrap and adding flash message

--- app/controllers/event_attendees_controller.rb
+++ app/controllers/event_attendees_controller.rb
@@ -3,33 +3,48 @@ class EventAttendeesController < ApplicationController
 	def index
 		@attendees = @event.attendees
 	end
+
 	def new
 		@attendee = @event.attendees.build		
 	end	
+
 	def create
 		@attendee = @event.attendees.build
-		@attendee.save
+		if @attendee.save
+			flash[:notice] = "新增參加人員"
+		   redirect_to event_attendees_path(@event,@attendee)
+		else 
+			flash[:alert] = "新增人員失敗"
+			render :action => :edit
+		end	
 
-		redirect_to event_attendees_path(@event,@attendee)
 	end
+
 	def show
 		@attendee = @event.attendees.find(params[:id]) 
 	end
+
 	def edit
 		@attendee = @event.attendees.find(params[:id])
 	end
+
 	def update
 		@attendee = @event.attendees.find(params[:id])
-		@attendee.update(attendees_params)
-
-		redirect_to event_attendee_path(@event)
+		if @attendee.update(attendees_params)
+			flash[:notice] = "更新成功"
+			redirect_to event_attendee_path(@event)
+		else
+			flash[:alert] = "更新失敗"
+			render :action => :edit
+		end	
 	end
 
 	def destroy
 		@attendee = @event.attendees.find(params[:id])
 		@attendee.destroy
-
+		flash[:alert] = "刪除成功"
 		redirect_to event_attendees_path(@event)
+
 	end
 
 
@@ -43,6 +58,7 @@ class EventAttendeesController < ApplicationController
 	def set_event
 		@event = Event.find(params[:event_id])
 	end
+
 	def attendees_params
 		params.require(:attendee).permit(:name, :event_id)
 	end
