33f84363aec542b87ba758c3f42b9fb8663d7b49
陳璟皓 <howard60915@gmail.com>
2016-09-29 21:39:46 +0800

    Explain has_many routes original variable

--- app/models/event.rb
+++ app/models/event.rb
@@ -5,7 +5,8 @@ class Event < ApplicationRecord
 	belongs_to :user
 	
 	delegate :name, :to => :category , :prefix => true, :allow_nil => true
-	has_many :event_groupships
+	
+	has_many :event_groupships #實際包含為 has_many :event_groupships, :class_name => "EventGroupships", :foreign_key => "event_id"
 	has_many :groups, :through => :event_groupships
 
 end
